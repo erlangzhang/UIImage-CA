eb943b37efd310f45a008f51ae5fbced8f731538
Charles <78268731@qq.com>
2015-11-12 15:27:41 +0800

    alter

--- UIImage+CA_Demo/UIImage+CA/UIImage+CA.m
+++ UIImage+CA_Demo/UIImage+CA/UIImage+CA.m
@@ -12,7 +12,8 @@
 #import <objc/objc-runtime.h>
 
 
-//4-->.png   4s-->.@2x.png   5(S)-->.568h@2x.png    6(s)-->.667h@2x.png   6P(s)-->@3x.png
+//4-->.png   4s-->@2x.png   5(S)-->-568h@2x.png    6(s)-->-667h@2x.png   6P(s)-->@3x.png
+//也可根据自己的需求定制修改
 
 #define __Device_Iphone_5__ (SCREEN_BOUNCE.size.height == 568)
 #define __Device_Iphone_6__ (SCREEN_BOUNCE.size.height == 667)
